e6cd69f09fab17abfbb39cadcd00088c21c3988b
Bryson McIver <brysonmciver@gmail.com>
2022-05-28 10:01:49 -0500

    FE

---
+++ epic-game-frontend/src/Components/SelectCharacter/index.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from "react";
+import "./SelectCharacter.css";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import myEpicGame from "../../utils/MyEpicGame.json";
+import LoadingIndicator from "../LoadingIndicator";
+
+const SelectCharacter = ({ setCharacterNFT }) => {
+  const [characters, setCharacters] = useState([]);
+  const [gameContract, setGameContract] = useState(null);
+  const [mintingCharacter, setMintingCharacter] = useState(false);
+
+  useEffect(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const gameContract = new ethers.Contract(
+        CONTRACT_ADDRESS,
+        myEpicGame.abi,
+        signer
+      );
+
+      setGameContract(gameContract);
+    }
+  }, []);
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      try {
+        const charactersTxn = await gameContract.getAllDefaultCharacters();
+        const characters = charactersTxn.map((characterData) =>
+          transformCharacterData(characterData)
+        );
+        setCharacters(characters);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    const onCharacterMint = async (sender, tokenId, characterIndex) => {
+      if (gameContract) {
+        const characterNFT = await gameContract.checkIfUserHasNFT();
+        setCharacterNFT(transformCharacterData(characterNFT));
+        alert(
+          `Your NFT is all done -- see it here: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+        );
+      }
+    };
+
+    if (gameContract) {
+      getCharacters();
+      gameContract.on("CharacterNFTMinted", onCharacterMint);
+    }
+
+    return () => {
+      if (gameContract) {
+        gameContract.off("CharacterNFTMinted", onCharacterMint);
+      }
+    };
+  }, [gameContract]);
+
+  const mintCharacterNFTAction = async (characterId) => {
+    try {
+      if (gameContract) {
+        setMintingCharacter(true);
+        const mintTxn = await gameContract.mintCharacterNFT(characterId);
+        await mintTxn.wait();
+        setMintingCharacter(false);
+      }
+    } catch (error) {
+      console.warn(error);
+      setMintingCharacter(false);
+    }
+  };
+
+  const renderCharacters = () =>
+    characters.map((character, index) => (
+      <div className="character-item" key={character.name}>
+        <div className="name-container">
+          <p>{character.name}</p>
+        </div>
+        <img src={character.imageURI} alt={character.name} />
+        <button
+          type="button"
+          className="character-mint-button"
+          onClick={() => mintCharacterNFTAction(index)}
+        >{`Mint ${character.name}`}</button>
+      </div>
+    ));
+
+  return (
+    <div className="select-character-container">
+      <h2>Mint You Hero. Choose Wisely.</h2>
+      {characters.length > 0 && (
+        <div className="character-grid">{renderCharacters()}</div>
+      )}
+      {mintingCharacter && (
+        <div className="loading">
+          <div className="indicator">
+            <LoadingIndicator />
+            <p>Minting....</p>
+          </div>
+          <img
+            src="https://media2.giphy.com/media/61tYloUgq1eOk/giphy.gif?cid=ecf05e47dg95zbpabxhmhaksvoy8h526f96k4em0ndvx078s&rid=giphy.gif&ct=g"
+            alt="Minting loading indicator"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SelectCharacter;
---
+++ epic-game-frontend/src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x555beD39fe954151B63457400229D702cf16a48d";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
